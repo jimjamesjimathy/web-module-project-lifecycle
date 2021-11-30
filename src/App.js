2d85835567457d519b498bec93d10e2648775f20
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2021-11-30 15:52:55 -0500

    Initial commit, forgot to do it earlier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import './components/Style.css';
+import axios from 'axios';
+import User from './components/User';
+import FollowerList from './components/FollowerList';
+
 
 class App extends React.Component {
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/jimjamesjimathy')
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    console.log('Go on Git: App rendering')
+    return(
+      <div className='App'>
+        <div className='header'>
+          <h1>Welcome to Go on GIT!</h1>
+          <form>
+            <input type='text' placeholder='Search for a user'/>
+            <button>search</button>
+          </form>
+        </div>
+        <img src='https://avatars.githubusercontent.com/u/85250373?v=4' width='300'></img>
+      </div>
+    );
   }
 }
 
---
+++ src/components/User.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class User extends React.Component {
+    render() {
+        return(
+            <div className='User'>
+                <img src='https://avatars.githubusercontent.com/u/85250373?v=4'></img>
+                <a target='_blank' href='https://github.com/jimjamesjimathy'></a>
+                <p>Total Repos: 69</p>
+                <p>Total Followers: 420</p>
+            </div>
+        )
+    }
+}
